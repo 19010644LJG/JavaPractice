7251f1ef1dbbbd01656b24716e254367ab62bdd9
이준구 <@169.254.133.32>
2017-05-11 21:29:52 +0900

    [실습과제 10] We Are the World !! 업그레이드

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ *<pre>
+ * org.dimigo.inheritance
+ * 	 |_PersonTest
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 8. 
+ * </pre>
+ * 
+ * @author : 이준구
+ * @version : 1.0
+ */
+public class PersonTest2{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Person[] Name = { 
+				new Person("Tom"),
+				new Korean("홍길동"),
+				new Japanese("다나카"),
+				new Chinese("왕밍")
+		};
+		
+		for(Person p : Name){
+			greeting(p);
+		}
+
+	}
+private static void greeting(Person p){
+			System.out.println(p.toString());
+			p.sayHello();
+			p.sayBye();
+			System.out.println();
+			}
+}
