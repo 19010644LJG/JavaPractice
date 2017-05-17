46562bf974d3c8a08f89f0fde3742ba3ff14aa4d
이준구 <@169.254.133.32>
2017-05-18 08:09:30 +0900

    [실습과제 12] 스마트폰 경쟁 업그레이드

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,44 @@
+/**
+
+ * 
+
+ */
+
+package org.dimigo.abstractclass;
+
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+
+	
+
+		SmartPhone phones[] = {
+
+			new IPhone("iPhone 7", "애플", 900000),
+
+			new Galaxy("갤럭시 S8", "삼성", 800000)
+
+		};
+
+		
+
+		for(SmartPhone phone : phones) {
+
+			System.out.println(phone);
+
+			phone.turnOn();
+
+			phone.pay();
+
+			phone.useSpecialFunction();
+
+			phone.turnOff();
+
+		}
+
+		}
+
+		
+
+		
+
+	}
